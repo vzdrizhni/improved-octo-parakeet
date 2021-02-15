3b82cb33ec8d9a02c86e346f188302847efa9d75
unknown <vzdrizhni@gmail.com>
2021-02-15 18:05:31 +0500

    Update the Day model

--- models/Day.js
+++ models/Day.js
@@ -1,10 +1,18 @@
 const mongo = require('mongoose');
 
+const Schema = mongo.Schema;
+
 const DaySchema = new mongo.Schema({
     name: {
         type: Date,
         required: true
-    }
+    },
+    meals: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Post'
+    }],
+}, {
+    timestamps: true
 })
 
 module.exports = mongo.model('Day', DaySchema);
