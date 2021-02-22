aac86f345dd12d2f66d5695b8e47f127ac33ce17
unknown <vzdrizhni@gmail.com>
2021-02-22 16:42:40 +0500

    Fix addFood controller to save food only if it was entered the first time

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useFindAndModify: true,
+        useFindAndModify: false,
         useUnifiedTopology: true
     });
 
--- controllers/food.js
+++ controllers/food.js
@@ -14,7 +14,10 @@ exports.getSingleFood = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Food with id ${foodId} was not found`, 404));
     }
 
-    res.status(200).json({success: true, data: food});
+    res.status(200).json({
+        success: true,
+        data: food
+    });
 })
 
 exports.createFood = asyncHandler(async (req, res, next) => {
@@ -26,24 +29,48 @@ exports.createFood = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Food was not craeted`, 404));
     }
 
-    res.status(201).json({success: true})
+    res.status(201).json({
+        success: true
+    })
 
 })
 
-exports.addFoodToTheMeal = async (req, res, next) => {
+exports.addFoodToTheMeal = asyncHandler(async (req, res, next) => {
     const mealId = req.params.mealId;
     let food;
+
     if (req.body._id) {
         food = await Food.findById(req.body._id);
-    }
-    
-    const isUniqueFood = await Food.find({name: req.body.name, calories: req.body.calories});
+    } else {
+        const isUniqueFood = await Food.find({
+            name: req.body.name,
+            calories: req.body.calories
+        }).countDocuments();
 
-    if (isUniqueFood.length === 0) {
-        food = await Food.create(req.body);
+        if (isUniqueFood === 0) {
+            food = await Food.create(req.body);
+        } else {
+            console.log(req.body);
+            food = await Food.findOne(req.body);
+        }
     }
 
-    let meal = await Meal.findByIdAndUpdate(mealId, {$push: {food: food}}).populate('food');
-    
-    res.json({success: true, meal})
-}
+    if (!food) {
+        return next(new ErrorResponse(`Food was not found`, 404));
+    }
+
+    let meal = await Meal.findByIdAndUpdate(mealId, {
+        $push: {
+            food: food
+        }
+    }).populate('food');
+
+    if (!meal) {
+        return next(new ErrorResponse(`Meal was not found`, 404));
+    }
+
+    res.json({
+        success: true,
+        data: meal
+    })
+})
