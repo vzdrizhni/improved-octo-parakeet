a18cf4cedeb304316ee56777e4e48625e0146eff
unknown <vzdrizhni@gmail.com>
2021-02-23 21:18:47 +0500

    Saved the value of the total calories in the pre-save hook

--- models/Meal.js
+++ models/Meal.js
@@ -13,30 +13,38 @@ const MealSchema = new mongoose.Schema({
     food: [{
         type: [mongoose.Schema.Types.ObjectId],
         ref: 'Food'
-    }]
+    }],
+    totalCalories: {
+        type: Number,
+        default: 0
+    }
 }, {
-    timestamps: true
+    timestamps: true,
 });
 
-MealSchema.statics.getAllCallories = async function() {
+MealSchema.statics.getAllCallories = async function () {
     console.log(this);
 
-    const obj = await this.aggregate([
-        {
-            $group: {
-                _id: '$food',
-                averageCost: {
-                    $sum: '$food.calories'
-                }
-            }
-        }
+    const obj = await this.aggregate([{
+            $unwind: "$food"
+        },
+        { $lookup: {from: 'meals', localField: 'food', foreignField: '_id', as: 'food'} } 
     ]);
 
     console.log(obj);
+
+    // obj.map(item => {
+    //     console.log(item.food);
+    // })
 };
 
-MealSchema.post('findOneAndUpdate', async function () {
-    await this.model.getAllCallories();
+MealSchema.pre('findOneAndUpdate', async function () {
+    // await this.model.getAllCallories();
+    const meal = await this.findOne(this);
+    const allCallories = meal.food.reduce((a, b) => {
+        return a.calories + b.calories
+    }, 0);
+    await this.updateOne({_id: meal._id}, {$set: {totalCalories: allCallories}});
 });
 
 module.exports = mongoose.model('Meal', MealSchema);
