ff736900cff126ffda400dfcb8b2e6648e47a59a
unknown <vzdrizhni@gmail.com>
2021-03-22 20:21:57 +0500

    Update user details model

--- models/UserDetails.js
+++ models/UserDetails.js
@@ -21,7 +21,23 @@ const UserDetailsSchema = new mongoose.Schema({
         type: Schema.Types.ObjectId,
         ref: 'User',
         required: true
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female']
+    },
+    age: {
+        type: Number,
+        required: true
     }
+});
+
+UserDetailsSchema.pre('save', async function () {
+    if (gender === 'male') {
+        this.normalWeight = (height - 100)*1.15
+    } else {
+        this.normalWeight = (height - 110)*1.15
+    }  
 });
 
 module.exports = mongoose.model('UserDetails', UserDetailsSchema);
